98b0e5b309b87e6cf2041922a5f375ce8d04d6b1
HarshitaN-WAL <harshita.n@westagilelabs.com>
2019-03-11 12:59:56 +0530

    change in config

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  let!(:project) { create(:project) }
-  let!(:user) { create(:user, :with_avatar) }
+RSpec.describe ProjectsController, type: :controller do
+  let!(:project) { create(:project) }  
+  let(:user) {create(:user)}
+  let(:user1) {create(:user)}
   before(:each) do
     allow(controller).to receive(:current_user).and_return(user)
   end
 
-  describe 'GET project' do
+  describe 'GET projects' do
     it 'should render index page' do
-
+      session[:user_id] = user.id
+      get :index
+      expect(response).to render_template(:index)
     end
   end
+
+  describe 'post create project' do
+    it 'should render show page' do
+      post :create, params: {"project"=>{"name"=>"Params", "start_date"=>"2019-03-11", "expected_target_date"=>"2019-03-14", "project_token"=>"2199776", "pt_token"=>"db582a94d248f6b17143351774a47847", "quality_token"=>"4a6076ef74c16d09d7cca6dbab264d89dc015eca6dbab264d89dc015eee", "github_slug"=>"HarshitaN-WAL/Dashboard"}, "user_id"=>[user.id]}
+      project = Project.last
+      expect(response).to redirect_to project_path(project)
+    end
+  end
+
+  describe 'NEW project' do
+    it 'should render show page' do
+      get :new
+      expect(assigns(:project)).to be_a_new(Project)
+    end
+  endend
+
+  describe 'GET projects#edit' do
+    it "should get the project" do
+    
+    end
+  end
+
 
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
-    association :project_users
+  factory :project do
     name {'monster'}
     start_date {'2019-02-25'}
     expected_target_date {'2019-03-25'}
     project_token {'2199776'}
     pt_token {'db582a94d248f6b17143351774a0000'}
-
+    quality_token {'4a6076ef74c16d09d7cca6dbab264d89dc015eee'}
+    github_slug {'HarshitaN-WAL/Dashboard'}
   end
 end
